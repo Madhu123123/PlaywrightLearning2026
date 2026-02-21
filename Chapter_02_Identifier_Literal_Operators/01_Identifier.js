1ca097b200a5a94fa7e3364f96bd27e4f126ee21
PlaywrightLearningBot <actions@users.noreply.github.com>
2026-02-21 06:59:27 +0530

    Update null type examples

---
+++ Chapter_02_Identifier_Literal_Operators/01_Identifier.js
@@ -0,0 +1,5 @@
+//let class = "Hello World"; // SyntaxError: Unexpected token 'class'
+//console.log(class);
+
+let full_name = `Hello World`;
+console.log(full_name);
---
+++ Chapter_02_Identifier_Literal_Operators/02_null_type.js
@@ -0,0 +1,14 @@
+let a = null;
+console.log(a); // Output: null
+let b;
+console.log(b); // Output: undefined
+
+
+console.log(a == b); // Output: true
+console.log(a === b); // Output: false
+
+console.log(5 == "5"); // Output: true
+console.log(5 === "5"); // Output: false
+
+console.log("" == "0"); // Output: false
+console.log("" === "0"); // Output: false
