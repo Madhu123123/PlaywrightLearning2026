d451f084d7b8eb8af48da306f64502cc03675271
PlaywrightLearningBot <actions@users.noreply.github.com>
2026-02-28 06:37:43 +0530

    Update chapter 06 loops code

--- chapter_06_Loops/01_Loops.js
+++ chapter_06_Loops/01_Loops.js
@@ -9,4 +9,11 @@ console.log(++b + ++b); // 11 + 12 = 23
 console.log(b); //12
 
 
+let a1 = 5;
+
+console.log(a1++ + ++a1 + ++a1 + a1++ ) // 5 + 7 +8 + 8 =28
+
+let b1 = 5
+console.log(b1-- + --b1 + --b1 + b1-- ) //5 + 3+ 2+2 = 12
+
 
